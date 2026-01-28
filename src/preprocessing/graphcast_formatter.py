3b4005c80ed297c2a555770ef2a3a54c89be5232
JianLi <44811760+jli-99@users.noreply.github.com>
2026-01-28 11:39:32 -0500

    debug

--- src/preprocessing/graphcast_formatter.py
+++ src/preprocessing/graphcast_formatter.py
@@ -457,6 +457,7 @@ class GraphCastFormatter:
     def process_single_date(self, date: datetime) -> List[Path]:
         """Process data for a single date"""
         logger.info(f"Processing date: {date}")
+        print("INSIDR" , date)
         
         # Load MCD data
         mcd_ds = self._load_mcd_files(date)
@@ -473,9 +474,10 @@ class GraphCastFormatter:
             mcd_ds_regridded,
             num_timesteps=self.config.num_input_steps
         )
+        print("PROCESSED DS", processed_ds.sizes)
         # Extend MCD time dimension (if needed)
         extend_ds = self._extend_time_dim(processed_ds, n_steps=self.config.num_output_steps)
-
+        print("EXTEND DS", extend_ds.sizes)
         # Align MCD time coordinates with ERA5
         #num_total_steps = self.config.num_input_steps + self.config.num_output_steps
         # mcd_ds = mcd_ds.assign_coords(time=era5_ds.time.values[:num_total_steps])
@@ -489,6 +491,7 @@ class GraphCastFormatter:
             res=self.config.target_resolution,
             steps=num_total_steps
         )
+        print("OUTPUT FILE", output_file)
         extend_ds.to_netcdf(output_file, mode='w')
         logger.info(f"Saved: {output_file}")
         # for i, hour in enumerate(hours):
